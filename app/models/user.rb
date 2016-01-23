53b67756008430b850fcd1d03fba6ce58e066234
JamieBolt <ppd2104@columbia.edu>
2016-01-23 16:13:31 -0500

    deleted users deletes their pins, ensure edit controls only show if user is live

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_many :pins
+  has_many :pins, dependent: :destroy
+#dependent destroy should ensure pins are deleted when their user is deleted
 
 #note devise automatically validates email and pass.
 #since we added name ourselves, have to add our own validator
